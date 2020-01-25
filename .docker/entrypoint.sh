17707ec74d21b5c841af7d88c35f6f45fcfedd67
zawiszaty <ZawiszaCzarny52@gmail.com>
2020-01-25 21:44:12 +0100

    another fix

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 if [ "${SKIP_COMPOSER_INSTALL:0}" != "1" ]; then
-	cd /var/www/html && php composer.phar global require hirak/prestissimo && php composer.phar install -n --prefer-dist -o && php bin/console ca:cl
+	cd /var/www/html && php composer.phar global require hirak/prestissimo && php composer.phar install -n --prefer-dist -o
 	lastExiteCode=$?
 	if [ "$lastExiteCode" != "0" ]; then exit $lastExiteCode; fi;
 fi;
